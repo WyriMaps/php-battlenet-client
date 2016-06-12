cf79ce1fac9bf7a90bb59317e1c279c2d0b0c0f6
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-06-12 16:34:42 +0200

    Async mounts example

---
+++ examples/mounts-async.php
@@ -0,0 +1,17 @@
+<?php
+
+use React\EventLoop\Factory;
+use Rx\Observer\CallbackObserver;
+use WyriMaps\BattleNet\AsyncClient;
+use WyriMaps\BattleNet\Resource\WorldOfWarcraft\MountInterface;
+
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+
+$loop = Factory::create();
+$client = new AsyncClient($loop);
+
+$client->worldOfWarcraft()->mounts()->subscribe(new CallbackObserver(function (MountInterface $mount) {
+    echo $mount->name(), PHP_EOL;
+}));
+
+$loop->run();
