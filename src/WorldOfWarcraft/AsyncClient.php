678bfcec58cd650bd7d07aee3e13fa9fbc2dd0ce
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-20 12:01:05 +0100

    List all pets

--- src/WorldOfWarcraft/AsyncClient.php
+++ src/WorldOfWarcraft/AsyncClient.php
@@ -3,6 +3,12 @@
 namespace WyriMaps\BattleNet\WorldOfWarcraft;
 
 use ApiClients\Foundation\Client;
+use ApiClients\Foundation\Hydrator\CommandBus\Command\HydrateCommand;
+use ApiClients\Foundation\Transport\CommandBus\Command\SimpleRequestCommand;
+use Psr\Http\Message\ResponseInterface;
+use Rx\Observable;
+use Rx\ObservableInterface;
+use Rx\React\Promise;
 
 class AsyncClient
 {
@@ -18,5 +24,21 @@ class AsyncClient
     public function __construct(Client $client)
     {
         $this->client = $client;
+    }
+
+    /**
+     * List all pets
+     *
+     * @return ObservableInterface
+     */
+    public function pets(): ObservableInterface
+    {
+        return Promise::toObservable(
+            $this->client->handle(new SimpleRequestCommand('wow/pet/'))
+        )->flatMap(function (ResponseInterface $response) {
+            return Observable::fromArray($response->getBody()->getJson()['pets']);
+        })->flatMap(function ($pet) {
+            return Promise::toObservable($this->client->handle(new HydrateCommand('WorldOfWarcraft\Pet', $pet)));
+        });
     }
 }
