adc13fb13ac7e6df8d5a53f891acc704f7a10b2e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-20 03:24:12 +0100

    Updated mount interface

--- src/Resource/WorldOfWarcraft/MountInterface.php
+++ src/Resource/WorldOfWarcraft/MountInterface.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace WyriMaps\BattleNet\Resource\WorldOfWarcraft;
 
@@ -7,45 +7,50 @@ use ApiClients\Foundation\Resource\ResourceInterface;
 interface MountInterface extends ResourceInterface
 {
     /**
-     * @return int
-     */
-    public function id() : int;
-    /**
      * @return string
      */
     public function name() : string;
+
     /**
      * @return int
      */
     public function spellId() : int;
+
     /**
      * @return int
      */
     public function creatureId() : int;
+
     /**
      * @return int
      */
     public function itemId() : int;
+
     /**
      * @return int
      */
     public function qualityId() : int;
+
     /**
      * @return string
      */
     public function icon() : string;
+
     /**
      * @return bool
      */
     public function isGround() : bool;
+
     /**
      * @return bool
      */
     public function isFlying() : bool;
+
     /**
      * @return bool
      */
     public function isAquatic() : bool;
+
     /**
      * @return bool
      */
