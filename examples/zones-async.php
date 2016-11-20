ed0c4704887335ed4ba7229dac1508e62025dd16
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-20 15:25:40 +0100

    Zones example

---
+++ examples/zones-async.php
@@ -0,0 +1,18 @@
+<?php
+
+use React\EventLoop\Factory;
+use Rx\Observer\CallbackObserver;
+use WyriMaps\BattleNet\AsyncClient;
+use WyriMaps\BattleNet\Resource\WorldOfWarcraft\ZoneInterface;
+use function ApiClients\Foundation\resource_pretty_print;
+
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+
+$loop = Factory::create();
+$client = new AsyncClient(require 'resolve_apikey.php', $loop);
+
+$client->worldOfWarcraft()->zones()->subscribe(new CallbackObserver(function (ZoneInterface $zone) {
+    resource_pretty_print($zone);
+}));
+
+$loop->run();
