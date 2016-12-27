1b672a5060ba1f626745256c4b868fbf02b38fe6
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-12-27 22:39:51 +0100

    Pets handler test

---
+++ tests/CommandBus/Handler/WorldOfWarcraft/PetsHandlerTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace WyriMaps\Tests\BattleNet\CommandBus\Handler\WorldOfWarcraft;
+
+use ApiClients\Tools\Services\Client\FetchAndIterateService;
+use ApiClients\Tools\TestUtilities\TestCase;
+use WyriMaps\BattleNet\CommandBus\Command\WorldOfWarcraft\PetsCommand;
+use WyriMaps\BattleNet\CommandBus\Handler\WorldOfWarcraft\PetsHandler;
+use WyriMaps\BattleNet\Resource\WorldOfWarcraft\PetInterface;
+
+final class PetsHandlerTest extends TestCase
+{
+    public function testBroadcasts()
+    {
+        $command = new PetsCommand();
+        $service = $this->prophesize(FetchAndIterateService::class);
+        $service->handle('wow/pet/', 'pets', PetInterface::HYDRATE_CLASS)->shouldBeCalled();
+        $handler = new PetsHandler($service->reveal());
+        $handler->handle($command);
+    }
+}
