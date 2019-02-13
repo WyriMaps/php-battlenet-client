cae983326fd5cdfc6b7ca66baa87a21f8cae2cc8
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-02-13 22:04:58 +0100

    Renamed apikey to apicred

---
+++ apicred.sample.php
@@ -0,0 +1,7 @@
+<?php
+
+return (function (): iterable {
+    // Your credentials from: https://develop.battle.net/access/clients
+    yield 'key' => 'YOUR_API_KEY';
+    yield 'secret' => 'YOUR_API_SECRET';
+})();
