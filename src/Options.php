302dbd4760e92f0ad6a2f830b56b56c731e3490d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-02-14 08:27:09 +0100

    Made all options constants public explicitly

--- src/Options.php
+++ src/Options.php
@@ -4,6 +4,6 @@ namespace WyriMaps\BattleNet;
 
 final class Options
 {
-    const API_KEY = 'api_key';
-    const API_SECRET = 'api_secret';
+    public const API_KEY = 'api_key';
+    public const API_SECRET = 'api_secret';
 }
