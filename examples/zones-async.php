35a4c75c846233b7cc5b7484d69c7241e9b58f36
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-02-16 18:12:56 +0100

    CS fixes

--- examples/zones-async.php
+++ examples/zones-async.php
@@ -1,7 +1,6 @@
 <?php declare(strict_types=1);
 use function ApiClients\Foundation\resource_pretty_print;
 use React\EventLoop\Factory;
-use WyriMaps\BattleNet\AsyncClient;
 use WyriMaps\BattleNet\Resource\WorldOfWarcraft\ZoneInterface;
 
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
